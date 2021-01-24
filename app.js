16a2d8bc4e06940c5a9de470cfeec447f81bc6fe
jmcia2020 <jeanmmcmahon@gmail.com>
2021-01-24 09:11:35 -0600

    trying to get js to work

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var imageElements = document.getElementsByTagName('img');
 
 var productIndex1 = 0;
 var productIndex2 = 1;
+var productIndex3 = 2;
 
 var rounds = 5;
 var allProducts = [];
@@ -72,22 +73,23 @@ function imageWasClicked(event){
     nextProductIndex2 = Math.floor(Math.random() * allProducts.length);
   }
 
+  var nextProductIndex3 = Math.floor(Math.random() * allProducts.length);
+  while((nextProductIndex3 === productIndex3) || (nextProductIndex3 === nextProductIndex2 || === nextProductIndex3)){
+    nextProductIndex2 = Math.floor(Math.random() * allProducts.length);
+  }
+
 
   //relfect the updates to the new values next pizzas
-  //set up reference to new pizza images.
+  //set up reference to new images.
   productIndex1 = nextProductIndex1;
   productIndex2 = nextProductIndex2;
-
-  imageElements[0].src = allProducts[productIndex1].imageUrl;
-  imageElements[1].src = allProducts[productIndex2].imageUrl;
-
+  productIndex3 = nextProductIndex3
 
   // For each of the three images, increment its property of times it has been shown by one.
-  productIndex1 = nextProductIndex1;
-  productIndex2 = nextProductIndex2;
 
   imageElements[0].src = allProducts[productIndex1].imageUrl;
   imageElements[1].src = allProducts[productIndex2].imageUrl;
+  imageElements[2].src = allProducts[productIndex3].imageUrl;
 
 
 if(totalClicks >= rounds){
@@ -95,12 +97,12 @@ if(totalClicks >= rounds){
     if(footerElement.firstElementChild){
       footerElement.firstElementChild.remove();
     }
-    footerElement.textContent = 'You chose 5 images. Thank you for your input!';
+    footerElement.textContent = 'You chose 5 products. Thank you for your input!';
   }//closing image was clicked.
 
 // Attach an event listener to the section of the HTML page where the images are going to be displayed.
 
 for(var i = 0; i < imageElements.length; i++){
-  console.log('this is the even listener for the click on pizza event.');
+  console.log("You chose a product and I'm listening.");
   imageElements[i].addEventListener('click', imageWasClicked);
 }
